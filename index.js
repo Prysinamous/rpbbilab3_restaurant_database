d9a93ff299faff76bc7ab27ddadf7a25cabd1ae5
Robertha <74151231+Prysinamous@users.noreply.github.com>
2022-02-10 23:53:44 -0500

    almost done put postman crashed

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const restRouter = require('./routes/restRouter.js');
 
+//Robbi AD 101236645
+
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('mongodb+srv://robbi:Panchitoisfat12@assignment2db.znhee.mongodb.net/wk4?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://robbi:Panchitoisfat12@Restaurants.znhee.mongodb.net/wk4?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(success => {
@@ -16,6 +18,6 @@ mongoose.connect('mongodb+srv://robbi:Panchitoisfat12@assignment2db.znhee.mongod
 });
 
 
-app.use(employeeRouter);
+app.use(restRouter);
 
 app.listen(8081, () => { console.log('Server is running...') });
